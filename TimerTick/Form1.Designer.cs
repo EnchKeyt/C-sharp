4cb8d38d5ea2ef1c708f786c2a5d2e51c76e9407
EnchKeyt <sametannpro@gmail.com>
2022-05-08 14:37:09 +0300

    Timer

---
+++ TimerTick/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TimerTick
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.minutsText = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.secondText = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // minutsText
+            // 
+            this.minutsText.AutoSize = true;
+            this.minutsText.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.minutsText.Location = new System.Drawing.Point(12, 9);
+            this.minutsText.Name = "minutsText";
+            this.minutsText.Size = new System.Drawing.Size(159, 58);
+            this.minutsText.TabIndex = 0;
+            this.minutsText.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(235, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 58);
+            this.label2.TabIndex = 1;
+            this.label2.Text = ":";
+            // 
+            // secondText
+            // 
+            this.secondText.AutoSize = true;
+            this.secondText.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.secondText.Location = new System.Drawing.Point(315, 9);
+            this.secondText.Name = "secondText";
+            this.secondText.Size = new System.Drawing.Size(159, 58);
+            this.secondText.TabIndex = 2;
+            this.secondText.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(554, 156);
+            this.Controls.Add(this.secondText);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.minutsText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label secondText;
+        private System.Windows.Forms.Label minutsText;
+    }
+}
+
---
+++ TimerTick/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Threading;
+
+namespace TimerTick
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DispatcherTimer dt = new DispatcherTimer();
+            dt.Interval = TimeSpan.FromSeconds(1);
+            dt.Tick += dispatcherTimer_Tick;
+
+            System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+
+            timer.Tick += new EventHandler(Tik);
+            timer.Interval = new TimeSpan(0, 2, 0);
+            timer.Start();
+            dt.Start();
+        }
+        public int minutes = 1;
+        public int second = 49;
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            minutsText.Text = minutes.ToString();
+            secondText.Text = second.ToString();
+            second--;
+            if (second == 0)
+            {
+                minutes--;
+                second = 59;
+                if (minutes == 0)
+                {
+                    MessageBox.Show("Время сеанса подходит к концу");
+                }
+            }
+
+
+
+        }
+        public void Tik(object sender, EventArgs e)
+        {
+          
+        }
+    }
+    
+}
