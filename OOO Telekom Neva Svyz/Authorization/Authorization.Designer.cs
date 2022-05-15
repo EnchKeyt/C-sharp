c879bae56e881654f488b2923e75f494322553f4
EnchKeyt <sametannpro@gmail.com>
2022-05-15 22:17:22 +0300

    Auth

---
+++ OOO Telekom Neva Svyz/Authorization/Authorization.Designer.cs
@@ -0,0 +1,207 @@
+﻿
+namespace Authorization
+{
+    partial class AuthorizationForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AuthorizationForm));
+            this.TitleLabel = new System.Windows.Forms.Label();
+            this.NumberLabel = new System.Windows.Forms.Label();
+            this.NumberTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordTextBox = new System.Windows.Forms.TextBox();
+            this.PasswordLabel = new System.Windows.Forms.Label();
+            this.CodeTextBox = new System.Windows.Forms.TextBox();
+            this.CodeLabel = new System.Windows.Forms.Label();
+            this.CancelButton = new System.Windows.Forms.Button();
+            this.EnterButton = new System.Windows.Forms.Button();
+            this.RepeatPictureBox = new System.Windows.Forms.PictureBox();
+            this.LogoPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.RepeatPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.TitleLabel.Location = new System.Drawing.Point(12, 69);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(227, 26);
+            this.TitleLabel.TabIndex = 1;
+            this.TitleLabel.Text = "Телеком Нева Связь";
+            // 
+            // NumberLabel
+            // 
+            this.NumberLabel.AutoSize = true;
+            this.NumberLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.NumberLabel.Location = new System.Drawing.Point(180, 168);
+            this.NumberLabel.Name = "NumberLabel";
+            this.NumberLabel.Size = new System.Drawing.Size(59, 20);
+            this.NumberLabel.TabIndex = 2;
+            this.NumberLabel.Text = "Номер";
+            // 
+            // NumberTextBox
+            // 
+            this.NumberTextBox.Location = new System.Drawing.Point(245, 168);
+            this.NumberTextBox.Name = "NumberTextBox";
+            this.NumberTextBox.Size = new System.Drawing.Size(173, 20);
+            this.NumberTextBox.TabIndex = 3;
+            this.NumberTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.NumberTextBox_KeyPress);
+            // 
+            // PasswordTextBox
+            // 
+            this.PasswordTextBox.Enabled = false;
+            this.PasswordTextBox.Location = new System.Drawing.Point(245, 194);
+            this.PasswordTextBox.Name = "PasswordTextBox";
+            this.PasswordTextBox.Size = new System.Drawing.Size(173, 20);
+            this.PasswordTextBox.TabIndex = 5;
+            this.PasswordTextBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.PasswordTextBox_KeyPress);
+            // 
+            // PasswordLabel
+            // 
+            this.PasswordLabel.AutoSize = true;
+            this.PasswordLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PasswordLabel.Location = new System.Drawing.Point(180, 194);
+            this.PasswordLabel.Name = "PasswordLabel";
+            this.PasswordLabel.Size = new System.Drawing.Size(67, 20);
+            this.PasswordLabel.TabIndex = 4;
+            this.PasswordLabel.Text = "Пароль";
+            // 
+            // CodeTextBox
+            // 
+            this.CodeTextBox.Enabled = false;
+            this.CodeTextBox.Location = new System.Drawing.Point(245, 220);
+            this.CodeTextBox.Name = "CodeTextBox";
+            this.CodeTextBox.Size = new System.Drawing.Size(147, 20);
+            this.CodeTextBox.TabIndex = 7;
+            // 
+            // CodeLabel
+            // 
+            this.CodeLabel.AutoSize = true;
+            this.CodeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CodeLabel.Location = new System.Drawing.Point(180, 220);
+            this.CodeLabel.Name = "CodeLabel";
+            this.CodeLabel.Size = new System.Drawing.Size(39, 20);
+            this.CodeLabel.TabIndex = 6;
+            this.CodeLabel.Text = "Код";
+            // 
+            // CancelButton
+            // 
+            this.CancelButton.AutoSize = true;
+            this.CancelButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(103)))), ((int)(((byte)(247)))));
+            this.CancelButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.CancelButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.CancelButton.Location = new System.Drawing.Point(245, 310);
+            this.CancelButton.Name = "CancelButton";
+            this.CancelButton.Size = new System.Drawing.Size(75, 27);
+            this.CancelButton.TabIndex = 9;
+            this.CancelButton.Text = "Отмена";
+            this.CancelButton.UseVisualStyleBackColor = false;
+            this.CancelButton.Click += new System.EventHandler(this.CancelButton_Click);
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.AutoSize = true;
+            this.EnterButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(74)))), ((int)(((byte)(103)))), ((int)(((byte)(247)))));
+            this.EnterButton.Enabled = false;
+            this.EnterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.EnterButton.Location = new System.Drawing.Point(343, 310);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(75, 27);
+            this.EnterButton.TabIndex = 10;
+            this.EnterButton.Text = "Вход";
+            this.EnterButton.UseVisualStyleBackColor = false;
+            // 
+            // RepeatPictureBox
+            // 
+            this.RepeatPictureBox.Enabled = false;
+            this.RepeatPictureBox.Image = global::Authorization.Properties.Resources.repeat;
+            this.RepeatPictureBox.ImageLocation = "";
+            this.RepeatPictureBox.Location = new System.Drawing.Point(398, 220);
+            this.RepeatPictureBox.Name = "RepeatPictureBox";
+            this.RepeatPictureBox.Size = new System.Drawing.Size(20, 20);
+            this.RepeatPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.RepeatPictureBox.TabIndex = 8;
+            this.RepeatPictureBox.TabStop = false;
+            // 
+            // LogoPictureBox
+            // 
+            this.LogoPictureBox.Image = global::Authorization.Properties.Resources.logo;
+            this.LogoPictureBox.ImageLocation = "";
+            this.LogoPictureBox.Location = new System.Drawing.Point(424, 12);
+            this.LogoPictureBox.Name = "LogoPictureBox";
+            this.LogoPictureBox.Size = new System.Drawing.Size(184, 154);
+            this.LogoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.LogoPictureBox.TabIndex = 0;
+            this.LogoPictureBox.TabStop = false;
+            // 
+            // AuthorizationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.CancelButton = this.CancelButton;
+            this.ClientSize = new System.Drawing.Size(620, 349);
+            this.Controls.Add(this.EnterButton);
+            this.Controls.Add(this.CancelButton);
+            this.Controls.Add(this.RepeatPictureBox);
+            this.Controls.Add(this.CodeTextBox);
+            this.Controls.Add(this.CodeLabel);
+            this.Controls.Add(this.PasswordTextBox);
+            this.Controls.Add(this.PasswordLabel);
+            this.Controls.Add(this.NumberTextBox);
+            this.Controls.Add(this.NumberLabel);
+            this.Controls.Add(this.TitleLabel);
+            this.Controls.Add(this.LogoPictureBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AuthorizationForm";
+            this.Text = "Авторизация";
+            this.Load += new System.EventHandler(this.AuthorizationForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.RepeatPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.LogoPictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox LogoPictureBox;
+        private System.Windows.Forms.Label TitleLabel;
+        private System.Windows.Forms.Label NumberLabel;
+        private System.Windows.Forms.TextBox NumberTextBox;
+        private System.Windows.Forms.TextBox PasswordTextBox;
+        private System.Windows.Forms.Label PasswordLabel;
+        private System.Windows.Forms.TextBox CodeTextBox;
+        private System.Windows.Forms.Label CodeLabel;
+        private System.Windows.Forms.PictureBox RepeatPictureBox;
+        private System.Windows.Forms.Button CancelButton;
+        private System.Windows.Forms.Button EnterButton;
+    }
+}
+
---
+++ OOO Telekom Neva Svyz/Authorization/Authorization.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Authorization
+{
+    public partial class AuthorizationForm : Form
+    {
+        public AuthorizationForm()
+        {
+            InitializeComponent();
+        }
+
+        private void AuthorizationForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        static DataRow authorizationDataRow;
+
+        public void NumberTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Convert.ToInt32(e.KeyChar) == 13)
+            {
+                if (NumberTextBox.Text == "")
+                {
+                    MessageBox.Show("Вы не ввели номер!");
+                    return;
+                }
+
+                try
+                {
+                    Convert.ToInt32(NumberTextBox.Text);
+                }
+
+                catch
+                {
+                    MessageBox.Show("Вводите только цифры!");
+                    return;
+                }
+
+                MySqlDataAdapter authorizationAdapter;
+                DataSet authorizationDataSet = new DataSet();
+                DataTable authorizationDataTable = new DataTable();
+
+                string connectionString = @"server=192.168.101.1;userid=ws01;password=1rNkX'%5;database=ws01;charset=utf8mb4;";
+                string sql = "SELECT * FROM users WHERE number = " + NumberTextBox.Text;
+
+                MySqlConnection connection = new MySqlConnection(connectionString);
+                authorizationAdapter = new MySqlDataAdapter(sql, connection);
+                authorizationAdapter.Fill(authorizationDataSet, "users");
+                authorizationDataTable = authorizationDataSet.Tables["users"];
+
+                try
+                {
+                    connection.Open();
+
+                    MessageBox.Show("Успешное подключение к базе данных!");
+
+                    connection.Close();
+                }
+                catch
+                {
+                    MessageBox.Show("При подключении к базе данных возникла ошибка!");
+                }
+
+                try
+                {
+                    authorizationDataRow = authorizationDataTable.Rows[0];
+
+                    PasswordTextBox.Enabled = true;
+                }
+
+                catch
+                {
+                    MessageBox.Show("Не верный номер!");
+                }
+            }
+        }
+
+        public void PasswordTextBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (Convert.ToInt32(e.KeyChar) == 13) { 
+                if (PasswordTextBox.Text == "")
+                {
+                    MessageBox.Show("Вы не ввели пароль!");
+                    return;
+                }
+
+                if (authorizationDataRow.Field<string>("password") == PasswordTextBox.Text)
+                {
+                    MessageBox.Show("Пароль правильный!");
+                }
+                else
+                {
+                    MessageBox.Show("Не верный пароль!");
+                }
+            }
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            NumberTextBox.Text = "";
+            PasswordTextBox.Text = "";
+            CodeTextBox.Text = "";
+
+            PasswordTextBox.Enabled = false;
+        }
+    }
+}
